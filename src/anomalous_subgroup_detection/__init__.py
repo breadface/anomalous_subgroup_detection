4a521f76edd0bcde183f4bc2cd1102818c34608e
jesmine <damilolao328@gmail.com>
2025-10-22 02:21:34 -0500

    Add __version__ attribute to package - Add version information to __init__.py for proper package metadata

--- src/anomalous_subgroup_detection/__init__.py
+++ src/anomalous_subgroup_detection/__init__.py
@@ -1,6 +1,8 @@
 from .detector import SubsetScanDetector
 from .scan_statistics import gaussian_scan_statistic, bernoulli_scan_statistic
 
+__version__ = "0.1.0"
+
 __all__ = [
     "SubsetScanDetector",
     "gaussian_scan_statistic",
